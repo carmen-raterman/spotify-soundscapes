b36a9a5c5f29f3bd004226cb52d62882b3f75ebf
carmen-raterman <ratersop@gmail.com>
2024-06-12 10:59:36 -0400

    added utils

---
+++ src/utils/reducer.js
@@ -0,0 +1,12 @@
+export const initialState = {
+    token: null, //null initially, but this is for spotify token
+};
+
+const reducer = (state, action) => {
+    switch(action.type) {
+        default:
+            return state;
+    }
+};
+
+export default reducer;
